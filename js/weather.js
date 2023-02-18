2ff3a4b01fde91474ec9f692ae6306cb7bdc3322
Nicholas Tillmann <ntillmann1439@gmail.com>
2023-02-18 15:41:31 -0500

    adding api req

--- js/weather.js
+++ js/weather.js
@@ -1,12 +1,21 @@
 // grab dom elements
 const searchBtn = document
   .getElementById("search-btn")
-  .addEventListener("click", getWeather);
-
-const input = document.getElementById("input");
+  .addEventListener("click", getInputWeather);
 
 console.log(input, searchBtn);
 
-function getWeather() {
-  console.log("button clicked");
+function getInputCity() {
+  const input = document.getElementById("input");
+  getCityWeather(input.value);
+}
+
+// fetch weather data from api
+function getCityWeather(city) {
+  // fetch data from an API that takes in a city name and returns the weather
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+  );
+  response = response.json();
+  console.log(response);
 }
